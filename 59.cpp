29642c0ca6e2abbf3468a37aeedcb0159a28a321
yash240804 <152909902+yash240804@users.noreply.github.com>
2024-09-14 20:28:30 +0530

    Add files via upload

---
+++ 59.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin>>n>>m;
+    cout<<sqrt(n)<<endl;
+    cout<<pow(n, 2)<<endl;
+    cout<<min(n, m)<<endl;
+    cout<<max(n, m)<<endl;
+    //tolower(); toupper();
+    return 0;
+}

d92352be6983291a15ca787e98f97677dfde39f3
yash240804 <yashbisen24@gmail.com>
2024-09-20 20:24:31 +0530

    Version 20

---
+++ 74.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+void insertionsort(int arr[], int size){
+    int index ;
+    for (int i = 0; i < size - 1; i++){
+        index = i;
+        for (int j = i + 1; j < size; j++){
+            if(arr[j]<arr[i]){
+                index = j;
+            }
+        }
+        swap(arr[i], arr[index]);
+    }
+}
+int main(){
+    int arr[1000];
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    insertionsort(arr, n);
+    for (int i = 0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}

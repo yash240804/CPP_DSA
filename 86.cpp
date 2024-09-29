04f1d30414825e5f1eb7032180c5882a1c93a8bc
yash240804 <yashbisen24@gmail.com>
2024-09-29 20:18:11 +0530

    Version 28

---
+++ 86.cpp
@@ -0,0 +1,11 @@
+// learned something new today
+/*
+vec can be a vectors or array
+vec.begin() to get the memory address of first element of array
+vec.end() to get the memory address of last element of array
+sort() to sort a array (Ex: sort(vec.begin(), vec.end()))
+vec.insert() to insert in a array
+vec.size() to get the size of array or vector
+vec.push_back() to insert in vector from back or end
+vec.pop_back() to delete in vector from back or end
+*/
